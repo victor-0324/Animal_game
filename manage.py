e91bf88696e192ecb96fdc32df645e63eb4d6ac9
vitor <xicovector@gmail.com>
2022-08-05 19:11:11 -0300

    Iniciando projeto

---
+++ manage.py
@@ -0,0 +1,8 @@
+from src import init_app
+
+app = init_app()
+
+
+# iniciar o Servidor
+if __name__ == "__main__":
+    app.run(debug=True , port=5001)
---
+++ src/main.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+
+def init_app():
+    app = Flask(__name__)
+
+    # Configuração do app
+    app.secret_key = "vitorvitoriaeyaramariaauvesdacosta"
+
+    # Database
+    # from .database import DBConnectionHendler
+    # from .database import Base
+
+    # db_connection = DBConnectionHendler()
+    # engine = db_connection.get_engine()
+
+    with app.app_context(): 
+
+         # Aplicativo inicial
+        from .blueprints import initial_app
+
+        app.register_blueprint(initial_app)
+
+        return app
